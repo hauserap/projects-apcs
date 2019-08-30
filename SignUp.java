db71ad1d85fbde7b4513bd1f24c7119b4ae564fd
Alex Hauser <hauserap@s.dcsdk12.org>
2019-08-30 12:40:20 -0600

    Create signup

---
+++ SignUp.java
@@ -0,0 +1,8 @@
+import java.util.System;
+
+public class Signup{
+
+    public static void main(String[] args){
+        
+    }
+}
--- TimeUntil.java
+++ TimeUntil.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class TimeUntil{
     public static void main(String [] args){
+
+        Scanner scan = new Scanner (LocalDateTime.now().getHour());
+
+
+
+
+
+
+        
+        
         
     }
 }
