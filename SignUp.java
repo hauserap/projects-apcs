01a8b82c826257599683c7a4476c3036a2381d03
Alex Hauser <hauserap@s.dcsdk12.org>
2019-08-30 12:52:18 -0600

    work on sign up

--- SignUp.java
+++ SignUp.java
@@ -1,8 +1,22 @@
-import java.util.System;
+import java.util.Scanner;
 
-public class Signup{
+public class SignUp{
 
     public static void main(String[] args){
-        
+        Scanner scanUsername = new Scanner (System.in);
+        System.out.println("Submit a username");
+
+        String inUserData = scanUsername.nextLine();
+
+        String takenUsername = "admin";
+        String takenUsername1 = "jimbob234";
+        String takenUsername2 = "skater_gurl17";
+        String takenUsername3 ="gary";
+
+        if(inUserData.equals(takenUsername)){
+            System.out.println("User name is taken");
+        }else{
+            System.out.println("Sounds good");
+        }
     }
 }
